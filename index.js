906af83bddc1338dfeebc46b0b68c1a305db6fd8
eugenejahn <eugenejahnjahn@gmail.com>
2018-02-17 21:16:47 -0800

    google sign in script

--- index.js
+++ index.js
@@ -121,5 +121,13 @@ function addData(title, content){
 	note_data_list.push(data);
 }
 
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 
 
