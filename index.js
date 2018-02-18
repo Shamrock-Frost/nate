58307c8516ccdc4a64f50742029c3882aae93219
eugenejahn <eugenejahnjahn@gmail.com>
2018-02-17 22:42:56 -0800

    test

--- index.js
+++ index.js
@@ -139,31 +139,3 @@ function onSignIn(googleUser) {
   	console.log(profile);
   }
 }
-
-
-function start() {
-  // 2. Initialize the JavaScript client library.
-  gapi.client.init({
-    'apiKey': 'gpHcyUUXCkKj5OMpv2nI3KcV',
-    // Your API key will be automatically added to the Discovery Document URLs.
-    'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
-    // clientId and scope are optional if auth is not required.
-    'clientId': '303214978933-qmqijqrsrb1kpbjidtc7msuougdn9fik.apps.googleusercontent.com',
-    'scope': 'profile',
-  }).then(function() {
-    // 3. Initialize and make the API request.
-    return gapi.client.people.people.get({
-      'resourceName': 'people/me',
-      'requestMask.includeField': 'person.names'
-    });
-  }).then(function(response) {
-    console.log(response.result);
-  }, function(reason) {
-    console.log('Error: ' + reason.result.error.message);
-  });
-};
-// 1. Load the JavaScript client library.
-gapi.load('client', start);
-
-
-
